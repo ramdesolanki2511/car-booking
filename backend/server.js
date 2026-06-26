2dd4e342740135f014ca1c8b387b7ee53536762d
ramde solanki <ramdesolanki2511@gmail.com>
2026-06-26 14:31:46 +0530

    Add npm module and server setup

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+
+const App = express();
+
+
+App.use(express.json());
+App.use(cookieParser())
+
+
+dotenv.config();
+const port = 3000;
+
+mongoose
+  .connect(process.env.mongo_uri)
+  .then(console.log("connected"))
+  .catch((error) => console.error(error));
+
+  
+
+App.listen(port, () => {
+  console.log("server listening !");
+});
+
+const allowedOrigins = ['http://localhost:5173'];
+
+App.use(
+  cors({
+    origin: allowedOrigins,
+    methods:['GET', 'PUT', 'POST' ,'PATCH','DELETE'],
+    credentials: true,
+  })
+);
+
+
+App.use('*', cloudinaryConfig);
+App.use("/api/user", userRoute);
+App.use("/api/auth", authRoute);
+App.use("/api/admin",adminRoute);
+App.use("/api/vendor",vendorRoute)
+
+
+
+App.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    succes: false,
+    message,
+    statusCode,
+  });
+});
